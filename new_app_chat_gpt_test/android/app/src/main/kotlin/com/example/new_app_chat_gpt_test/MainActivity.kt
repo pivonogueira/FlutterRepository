e65b3c780abd39094705cd2b059a63bf02dc5ba4
Pedro Ivo <97911613+pivonogueira@users.noreply.github.com>
2024-07-05 20:59:27 -0300

    Removendo arquivos não utilizados

--- new_app_chat_gpt_test/android/app/src/main/kotlin/com/example/new_app_chat_gpt_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.new_app_chat_gpt_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- untitled_test/android/app/src/main/kotlin/com/example/untitled_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.untitled_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
